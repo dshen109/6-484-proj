a09a15c5481a1d4c47ad157fe5b45b5412e687d1
Daniel Shen <dshen109@gmail.com>
2022-03-28 13:01:49 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='deep_hvac',
+    version='0.0.0',
+    description='HVAC control using deep RL',
+    author='Daniel Shen & Cale Gregory',
+    packages=find_packages(),
+    python_requires='>=3.8',
+    install_requires=[
+        'pandas',
+        'openpyxl'
+        ],
+    extras_require={
+        'test': ['pytest', 'mock'],
+    },
+    project_urls={},
+)
