5792eef86e78adf63d05dacc6f5dd80097ba7b65
cgregory2500@gmail.com <cale@dhcp-10-29-71-246.dyn.MIT.EDU>
2022-04-13 02:55:07 -0400

    updated scripts

--- scripts/comparison.py
+++ scripts/comparison.py
@@ -12,20 +12,20 @@ sys.path.insert(2, hvac_dir)
 from naive import naive_agent, make_default_env, update_results
 from run_ppo import make_ppo_agent
 
-def run_episode(agent, env, episode_steps):
-    env.reset()
+def run_episode(agent, env, episode_steps, time=0):
+    env.reset(time=time)
     for _ in range(episode_steps):
         action = agent.get_action(env.get_obs())
         env.step(action[0])
     return env.results
 
-def get_results(agent, env, episode_steps=1024, max_steps=100000):
+def get_results(agent, env, episode_steps=1024, max_steps=500000, time=0):
 
     results = defaultdict(list)
     total_steps = 0
     
     while total_steps < max_steps:
-        ep_results = run_episode(agent, env, episode_steps)
+        ep_results = run_episode(agent, env, episode_steps, time=time)
         update_results(results, ep_results)
         total_steps += episode_steps
 
@@ -40,14 +40,14 @@ if __name__ == "__main__":
     naive_results = naive_agent()
 
     # run steps and collect results on ppo_agent
-    agent_ppo, _ =  make_ppo_agent(max_steps = 100000)
-    ppo_results = get_results(agent_ppo, env)
+    agent_ppo, _ =  make_ppo_agent(max_steps = 1000000)
+    ppo_results = get_results(agent_ppo, env, time=6048)
 
     price_dict = {'PPO Price':[[i for i in range(1024)], list(np.mean(np.array(ppo_results['price']), axis=0))],
                   'Naive Price': [[i for i in range(1024)], list(np.mean(np.array(naive_results['price']), axis=0))]}
 
-    air_dict = {'PPO Air':[[i for i in range(1024)], list(np.mean(np.array(ppo_results['t_air']), axis=0))],
-                  'Naive Air': [[i for i in range(1024)], list(np.mean(np.array(naive_results['t_air']), axis=0))]}
+    air_dict = {'PPO Air':[[i for i in range(1024)], list(np.mean(np.array(ppo_results['t_inside']), axis=0))],
+                  'Naive Air': [[i for i in range(1024)], list(np.mean(np.array(naive_results['t_inside']), axis=0))]}
 
     settings_dict = {'PPO Heating':[[i for i in range(1024)], list(np.mean(np.array(ppo_results['set_heating']), axis=0))],
                   'Naive Heating': [[i for i in range(1024)], list(np.mean(np.array(naive_results['set_heating']), axis=0))],
@@ -58,3 +58,8 @@ if __name__ == "__main__":
     plot_curves(price_dict, "", xlabel='Step', ylabel='Price')
     plot_curves(air_dict, "", xlabel='Step', ylabel='Temp')
     plot_curves(settings_dict, "", xlabel='Step', ylabel='Temp')
+
+    air_dict = {'PPO Air':[[i for i in range(1024)], list(np.mean(np.array(ppo_results['t_air']), axis=0))],
+                  'Naive Air': [[i for i in range(1024)], list(np.mean(np.array(naive_results['t_air']), axis=0))]}
+
+    plot_curves(air_dict, "", xlabel='Step', ylabel='Temp')
