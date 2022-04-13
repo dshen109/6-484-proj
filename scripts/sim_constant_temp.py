3f7812dde7ce451761b4ad06e5d49c602f895ebc
Daniel Shen <dshen109@gmail.com>
2022-04-13 11:04:22 -0400

    constant temp sim

--- scripts/sim_constant_temp.py
+++ scripts/sim_constant_temp.py
@@ -3,9 +3,14 @@ import sys
 
 from matplotlib import pyplot as plt
 import numpy as np
+import pandas as pd
 
+from deep_hvac import logger
 from deep_hvac.naive import naive_agent
-from deep_hvac.naive import make_default_env
+from deep_hvac.simulator import SimEnv
+from deep_hvac.util import NsrdbReader, ErcotPriceReader
+from rc_simulator.building_physics import Zone
+from rc_simulator.radiation import Window
 
 
 hvac_dir = os.path.join(
@@ -26,8 +31,38 @@ def plot(data, title, xlabel='steps', ylabel='reward'):
     plt.show()
 
 
+def make_env():
+    datadir = os.path.join(
+        os.path.split(os.path.abspath(__file__))[0],
+        '..', 'data'
+    )
+    logger.debug("Loading NSRDB data...")
+    nsrdb = NsrdbReader(os.path.join(datadir, '1704559_29.72_-95.35_2018.csv'))
+    logger.debug("Finished loading NSRDB data.")
+    logger.debug("Loading Houston price data...")
+    prices = pd.read_pickle(
+        os.path.join(datadir, 'houston-2018-prices.pickle')
+    )
+    logger.debug("Finished loading price data.")
+    window_area = 10
+    office = Zone(window_area=window_area)
+    south_window = Window(azimuth_tilt=0, altitude_tilt=90, area=window_area)
+
+    latitude, longitude = 29.749907, -95.358421
+
+    env = SimEnv(prices=prices,
+                 weather=nsrdb.weather_hourly,
+                 agent=None,
+                 coords=[latitude, longitude],
+                 zone=office,
+                 windows=[south_window]
+    )
+
+    return env
+
+
 if __name__ == '__main__':
-    env = make_default_env()
+    env = make_env()
     naive_results = naive_agent(heating_temp=30, cooling_temp=35)
 
     prices = [[i for i in range(1024)], list(np.mean(np.array(naive_results['price']), axis=0))]
