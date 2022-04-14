2bd0fe49724e8184fc64548f6f1d01192c0459c4
Daniel Shen <dshen109@gmail.com>
2022-04-14 13:50:03 -0400

    clean up sim stepping

--- deep_hvac/simulator.py
+++ deep_hvac/simulator.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 import random
+from types import SimpleNamespace
 
 from gym import Env, spaces
 import pandas as pd
@@ -7,6 +8,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from deep_hvac.util import sun_position
+
+
+class SimConfig(SimpleNamespace):
+
+    def __init__(self, *args, **kwargs):
+        pass
 
 
 class SimEnv(Env):
@@ -29,12 +36,13 @@ class SimEnv(Env):
         super(SimEnv, self).__init__()
 
         inf = float('inf')
-        self.observation_space = spaces.Box(low=np.array([-inf, -inf, -inf, -inf, -inf, -inf]),
-                                            high=np.array([inf, inf, inf, inf, inf, inf]),
-                                            dtype=np.float32)
-        # First element is heating setpoint, second element is cooling setpoint.
-        self.action_space = spaces.Box(low=np.array([-10, -10]),
-                                       high=np.array([10, 10]),
+        self.observation_space = spaces.Box(
+            low=np.array([-inf, -inf, -inf, -inf, -inf, -inf]),
+            high=np.array([inf, inf, inf, inf, inf, inf]),
+            dtype=np.float32)
+        # First element is heating stpt, second element is cooling stpt.
+        self.action_space = spaces.Box(low=np.array([10, 10]),
+                                       high=np.array([40, 40]),
                                        dtype=np.float32)
 
         self.action_space
@@ -51,7 +59,11 @@ class SimEnv(Env):
         self.reset()
 
     def reset(self, time=5024):
-        # reset time to a random time in the year
+        """Reset the simulator to a certain time of the year.
+
+        :param int time:
+        """
+        # reset time to a time in the year
         # TODO: Reset to a random time that guarantees a full episode
         self.time = time
         self.timestep = 0
@@ -63,26 +75,22 @@ class SimEnv(Env):
         self.zone.t_set_cooling = 25
 
         # reset the temperature of the building mass
-        self.t_m_prev = random.randint(15, 25)
+        self.t_m_prev = (self.zone.t_set_heating + self.zone.t_set_cooling) / 2
         self.step_bulk()
-        timestamp, cur_weather = self.get_timestamp_and_weather()
-        self.cur_state = [
-            self.zone.t_set_heating,
-            self.zone.t_set_cooling,
-            cur_weather['Temperature'],
-            self.t_m_prev,
-            timestamp.hour,
-            timestamp.weekday()
-        ]
 
         # reset the episodes results
         self.results = defaultdict(list)
 
-        return self.cur_state
+        return self.get_state()
 
     def step_bulk(self):
-        """Step through the building simulator and update the bulk building
-        mass."""
+        """Run building simulator to update the bulk building
+        mass.
+
+        :return:
+            - timestamp that was updated
+            - t_out outdoor air temperature.
+        """
 
         timestamp, cur_weather = self.get_timestamp_and_weather()
 
@@ -90,13 +98,15 @@ class SimEnv(Env):
             self.latitude, self.longitude, timestamp)
 
         for window in self.windows:
+            # Window simulator actually takes in Wm**2 not Whm**2 like the
+            # documentation says.
             window.calc_solar_gains(
                 sun_altitude=altitude, sun_azimuth=azimuth,
-                normal_direct_radiation=cur_weather['DNI_Whm2'],
-                horizontal_diffuse_radiation=cur_weather['DHI_Whm2']
+                normal_direct_radiation=cur_weather['DNI_Wm2'],
+                horizontal_diffuse_radiation=cur_weather['DHI_Wm2']
             )
 
-        t_out = cur_weather['Temperature']
+        t_out = self.get_temperature(self.time)
 
         self.zone.solve_energy(
             internal_gains=0,
@@ -114,33 +124,37 @@ class SimEnv(Env):
         """
         :param Array of size two to determine new setpoint for heating and
         cooling temperature respectively
+
+        :return: state, reward, done, info
         """
         self.zone.t_set_heating = action[0]
         self.zone.t_set_cooling = action[1]
 
+        # timestamp is the t time
         timestamp, t_out = self.step_bulk()
 
-        for attr in ('heating_demand', 'heating_energy', 'cooling_demand',
-                     'cooling_energy', 'electricity_out', 't_air'):
+        for attr in ('heating_demand', 'heating_sys_electricity',
+                     'cooling_demand', 'cooling_sys_electricity',
+                     'electricity_out', 't_air'):
             self.results[attr].append(getattr(self.zone, attr))
-        self.results['t_inside'].append(self.t_m_prev)
+        self.results['t_inside'].append(self.zone.t_air)
+        self.results['timestamp'].append(self.get_timestamp(self.time - 1))
 
         self.results['solar_gain'].append(
             sum([window.solar_gains for window in self.windows]))
 
         # Calculate all electricity consumed and price
         price = self.get_avg_hourly_price()
-        elec_consumed = self.zone.heating_energy + self.zone.cooling_energy
+        elec_consumed = (
+            self.zone.heating_sys_electricity +
+            self.zone.cooling_sys_electricity
+        )
         self.results['electricity_consumed'].append(elec_consumed)
         self.results['price'].append(elec_consumed * price)
 
-        self.cur_state = [self.zone.t_set_heating,
-                         self.zone.t_set_cooling,
-                         t_out,
-                         self.t_m_prev,
-                         timestamp.hour,
-                         timestamp.weekday()]
-        reward, info = self.get_reward(elec_consumed * price, self.t_m_prev)
+        # t_out, timestamp.hour and weekday are for the t+1 time.
+        # t_set_heating, t_set_cooling, t_air is for the t time
+        reward, info = self.get_reward(elec_consumed * price, self.zone.t_air)
         self.ep_reward += reward
         self.results['reward'].append(reward)
         self.results['set_heating'].append(self.zone.t_set_heating)
@@ -148,15 +162,34 @@ class SimEnv(Env):
         self.timestep += 1
         info['success'] = False
 
-        return self.cur_state, reward, self.timestep >= 1024, info
+        return self.get_state(), reward, self.timestep >= 1024, info
 
     def plot_results(self, attr='t_air'):
         annual_results = pd.DataFrame(self.results)
         annual_results[[attr]].plot()
         plt.show()
 
+    def get_state(self):
+        """If self.time is at index t, returns:
+
+            - t_set_heating for t-1
+            - t_set_cooling for t-1
+            - t_out for t
+            - t_air for t-1
+            - hour for t
+            - weekday for t
+        """
+        return [
+            self.zone.t_set_heating,
+            self.zone.t_set_cooling,
+            self.get_temperature(self.time),
+            self.zone.t_air,
+            self.get_timestamp(self.time).hour,
+            self.get_timestamp(self.time).weekday()
+        ]
+
     def get_obs(self):
-        return self.cur_state
+        return self.get_state()
 
     def get_avg_hourly_price(self):
         '''
@@ -172,8 +205,13 @@ class SimEnv(Env):
 
         :return: timestamp, weather data.
         """
-        row = self.weather.reset_index().iloc[self.time]
-        return row[0], row[1:]
+        return self.get_timestamp(self.time), self.weather.iloc[self.time]
+
+    def get_timestamp(self, index):
+        return self.weather.index[index]
+
+    def get_temperature(self, index):
+        return self.weather.iloc[index].loc['Temperature']
 
     def get_reward(self, price, t_air):
         """Return tuple of reward and info dict.
