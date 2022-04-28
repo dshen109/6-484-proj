dedb34747439425244ac3b5cfff44f9af4cd5a4b
Daniel Shen <dshen109@gmail.com>
2022-04-28 10:51:18 -0400

    add test env setup

--- deep_hvac/runner.py
+++ deep_hvac/runner.py
@@ -100,7 +100,77 @@ def make_default_env(episode_length=24 * 30, terminate_on_discomfort=True,
         logger.debug("Running naive baseline...")
         obs = env.reset(0)
         for _ in range(365 * 24 - 2):
-            obs, _, terminate, _ = env.step(naive_agent.get_action(obs)[0])
+            obs, _, _, _ = env.step(naive_agent.get_action(obs)[0])
+        costs = env.results['electricity_cost']
+        # pad with zeros
+        costs.insert(0, 0)
+        costs.append(0)
+        env_args['expert_performance'] = pd.DataFrame(
+            costs, columns=['cost'], index=nsrdb.weather_hourly.index)
+        env.results['expert_performance'] = env_args['expert_performance']
+        pd.to_pickle(env.results, 'data/results-expert.pickle')
+    elif isinstance(expert_performance, str):
+        expert = pd.read_pickle(expert_performance)
+        if not isinstance(expert, pd.DataFrame):
+            env_args['expert_performance'] = expert['expert_performance']
+        else:
+            env_args['expert_performance'] = expert
+    else:
+        env_args['expert_performace'] = expert_performance
+    if env_name in registry.env_specs:
+        del registry.env_specs[env_name]
+    register(
+        id=env_name, entry_point='deep_hvac.simulator:SimEnv', kwargs=env_args
+    )
+    return make_vec_env(env_name, 1, 0).envs[0], env_name
+
+
+def make_testing_env(episode_length=24 * 30, terminate_on_discomfort=False,
+                     discomfort_penalty=1e4, discrete_action=True,
+                     expert_performance=None):
+    """Create 2019 Houston dataset as testing environment."""
+    if discrete_action:
+        env_name = 'DefaultTestBuilding-v0-action-discrete'
+    else:
+        env_name = 'DefaultTestBuilding-v0-action-continuous'
+    logger.debug(f"Creating default test environment {env_name}.")
+    config = simulator.SimConfig(
+        episode_length=episode_length,
+        terminate_on_discomfort=False,
+        discomfort_penalty=discomfort_penalty,
+        discrete_action=discrete_action
+    )
+    datadir = os.path.join(
+        os.path.split(os.path.abspath(__file__))[0],
+        '..', 'data'
+    )
+    logger.debug("Loading NSRDB data...")
+    nsrdb = NsrdbReader(os.path.join(datadir, '1704559_29.72_-95.35_2019.csv'))
+    logger.debug("Finished loading NSRDB data.")
+    logger.debug("Loading Houston price data...")
+    ercot = pd.read_pickle(os.path.join(
+        datadir, 'houston-2019-prices.pickle'))
+    logger.debug("Finished loading price data.")
+    zone, windows, latitude, longitude = default_building()
+
+    env_args = dict(
+        prices=ercot,
+        weather=nsrdb.weather_hourly,
+        agent=None,
+        coords=(latitude, longitude),
+        zone=zone,
+        windows=windows,
+        config=config,
+        expert_performance=None
+    )
+    if expert_performance is None:
+        # Run the naive agent as a baseline.
+        naive_agent = agent.NaiveAgent()
+        env = simulator.SimEnv(**env_args)
+        logger.debug("Running naive baseline...")
+        obs = env.reset(0)
+        for _ in range(365 * 24 - 2):
+            obs, _, _, _ = env.step(naive_agent.get_action(obs)[0])
         costs = env.results['electricity_cost']
         # pad with zeros
         costs.insert(0, 0)
@@ -122,22 +192,3 @@ def make_default_env(episode_length=24 * 30, terminate_on_discomfort=True,
         id=env_name, entry_point='deep_hvac.simulator:SimEnv', kwargs=env_args
     )
     return make_vec_env(env_name, 1, 0).envs[0], env_name
-
-
-def make_testing_env(episode_length=24 * 30, terminate_on_discomfort=False,
-                     discomfort_penalty=100):
-    """Create 2019 Houston dataset as testing environment."""
-    # TODO: Finish.
-    datadir = os.path.join(
-        os.path.split(os.path.abspath(__file__))[0],
-        '..', 'data'
-    )
-    logger.debug("Loading NSRDB data...")
-    nsrdb = NsrdbReader(os.path.join(datadir, '1704559_29.72_-95.35_2019.csv'))
-    logger.debug("Finished loading NSRDB data.")
-    logger.debug("Loading Houston price data...")
-    ercot = pd.read_pickle(os.path.join(
-        datadir, 'houston-2018-prices.pickle'))
-    logger.debug("Finished loading price data.")
-    # TODO: finish.
-
