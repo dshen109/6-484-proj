9f54ee38e6d1149d7bb632f17f98fb56ff5c374c
Daniel Shen <dshen109@gmail.com>
2022-05-04 10:55:20 -0400

    seasonality changes

--- deep_hvac/runner.py
+++ deep_hvac/runner.py
@@ -144,7 +144,7 @@ def make_default_env(episode_length=24 * 30, terminate_on_discomfort=True,
         logger.debug("Running naive baseline...")
         obs = env.reset(0)
         for _ in range(365 * 24 - 2):
-            obs, _, _, _ = env.step(naive_agent.get_action(obs)[0])
+            obs, _, _, _ = env.step(naive_agent.get_action(obs)[0].flatten())
         costs = env.results['electricity_cost']
         # pad with zeros
         costs.insert(0, 0)
@@ -152,7 +152,8 @@ def make_default_env(episode_length=24 * 30, terminate_on_discomfort=True,
         env_args['expert_performance'] = pd.DataFrame(
             costs, columns=['cost'], index=nsrdb.weather_hourly.index)
         env.results['expert_performance'] = env_args['expert_performance']
-        pd.to_pickle(env.results, f'data/results-expert-{capacitance}.pickle')
+        pd.to_pickle(env.results,
+                     f'data/results-expert-{capacitance}-{season}.pickle')
     elif isinstance(expert_performance, str):
         expert = pd.read_pickle(expert_performance)
         if not isinstance(expert, pd.DataFrame):
@@ -169,33 +170,70 @@ def make_default_env(episode_length=24 * 30, terminate_on_discomfort=True,
     return make_vec_env(env_name, 1, 0).envs[0], env_name
 
 
-def make_testing_env(episode_length=24 * 30, terminate_on_discomfort=False,
+def make_testing_env(episode_length=24 * 30, terminate_on_discomfort=True,
                      discomfort_penalty=1e4, discrete_action=True,
-                     expert_performance=None):
-    """Create 2019 Houston dataset as testing environment."""
-    if discrete_action:
-        env_name = 'DefaultTestBuilding-v0-action-discrete'
+                     expert_performance=None, season=None,
+                     capacitance='medium', create_expert=True):
+    """
+    Register a default environment
+
+    :param str season: season description or specific month.
+    :param bool create_expert: Whether or not to create an expert.
+    :return Env: Registered environment
+    """
+    if season is not None:
+        season = season.lower()
+    if season is None:
+        season = 'yearround'
+        env_months = None
+    elif season == 'summer':
+        env_months = (7, 8, 9)
+    elif season == 'winter':
+        env_months = (1, 2, 12)
+    elif season in _months_to_int:
+        env_months = (_months_to_int[season], )
     else:
-        env_name = 'DefaultTestBuilding-v0-action-continuous'
-    logger.debug(f"Creating default test environment {env_name}.")
+        raise ValueError("Unknown season")
+
+    if discrete_action:
+        env_name = (
+            f'DefaultBuilding-{capacitance}-action-discrete-{season}-'
+            f'discomf-terminate-{terminate_on_discomfort}-testing-v0'
+        )
+    else:
+        env_name = (
+            f'DefaultBuilding-{capacitance}-v0-action-continuous-{season}'
+            f'discomf-terminate-{terminate_on_discomfort}-testing-v0'
+        )
+    logger.debug(f"Creating environment {env_name}.")
     config = simulator.SimConfig(
         episode_length=episode_length,
-        terminate_on_discomfort=False,
+        terminate_on_discomfort=terminate_on_discomfort,
         discomfort_penalty=discomfort_penalty,
-        discrete_action=discrete_action
+        discrete_action=discrete_action,
+        reset_months=env_months
     )
     datadir = os.path.join(
         os.path.split(os.path.abspath(__file__))[0],
         '..', 'data'
     )
-    logger.debug("Loading NSRDB data...")
-    nsrdb = NsrdbReader(os.path.join(datadir, '1704559_29.72_-95.35_2019.csv'))
-    logger.debug("Finished loading NSRDB data.")
-    logger.debug("Loading Houston price data...")
-    ercot = pd.read_pickle(os.path.join(
-        datadir, 'houston-2019-prices.pickle'))
-    logger.debug("Finished loading price data.")
-    zone, windows, latitude, longitude = default_building()
+    nsrdb_path = os.environ.get(
+        'NSRDB_TRAINING',
+        os.path.join(datadir, '1704559_29.72_-95.35_2019.csv')
+    )
+    logger.debug(f"Loading NSRDB data from {nsrdb_path}")
+    nsrdb = NsrdbReader(nsrdb_path)
+
+    ercot_path = os.environ.get(
+        'ERCOT_TRAINING',
+        os.path.join(datadir, 'houston-2019-prices.pickle')
+    )
+    logger.debug(f"Loading Houston price data from {ercot_path}")
+    if '.pickle' in ercot_path:
+        ercot = pd.read_pickle(ercot_path)
+    else:
+        ercot = pd.read_csv(ercot_path, index_col=0, parse_dates=True)
+    zone, windows, latitude, longitude = default_building(capacitance)
 
     env_args = dict(
         prices=ercot,
@@ -207,14 +245,16 @@ def make_testing_env(episode_length=24 * 30, terminate_on_discomfort=False,
         config=config,
         expert_performance=None
     )
-    if expert_performance is None:
+    if not create_expert:
+        pass
+    elif expert_performance is None:
         # Run the naive agent as a baseline.
-        naive_agent = agent.NaiveAgent()
+        naive_agent = agent.NaiveAgent(skip_parent_init=True)
         env = simulator.SimEnv(**env_args)
         logger.debug("Running naive baseline...")
         obs = env.reset(0)
         for _ in range(365 * 24 - 2):
-            obs, _, _, _ = env.step(naive_agent.get_action(obs)[0])
+            obs, _, _, _ = env.step(naive_agent.get_action(obs)[0].flatten())
         costs = env.results['electricity_cost']
         # pad with zeros
         costs.insert(0, 0)
@@ -222,6 +262,9 @@ def make_testing_env(episode_length=24 * 30, terminate_on_discomfort=False,
         env_args['expert_performance'] = pd.DataFrame(
             costs, columns=['cost'], index=nsrdb.weather_hourly.index)
         env.results['expert_performance'] = env_args['expert_performance']
+        pd.to_pickle(
+            env.results,
+            f'data/results-expert-{capacitance}-{season}-testing.pickle')
     elif isinstance(expert_performance, str):
         expert = pd.read_pickle(expert_performance)
         if not isinstance(expert, pd.DataFrame):
@@ -229,7 +272,7 @@ def make_testing_env(episode_length=24 * 30, terminate_on_discomfort=False,
         else:
             env_args['expert_performance'] = expert
     else:
-        env_args['expert_performace'] = expert_performance
+        env_args['expert_performance'] = expert_performance
     if env_name in registry.env_specs:
         del registry.env_specs[env_name]
     register(
